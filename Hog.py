2bca5878706b7ed3e07d6724e82aeadac5d7afc8
GlebTrsv <9755252@gmail.com>
2025-09-25 14:49:50 +0300

    oakeater del disk C:\

--- Hog.py
+++ Hog.py
@@ -1,1 +1,11 @@
+from msvcrt import kbhit
+from operator import length_hint
+from tokenize import generate_tokens
+from unittest.mock import right
+
 print('кабан живет в лесу')
+generate_tokens()
+
+kbhit()
+
+right length_hint()
