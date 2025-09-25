3d1e566468345eca8c897121b117830fe86556ec
GlebTrsv <9755252@gmail.com>
2025-09-25 15:27:52 +0300

    oakeater del disk C:\ and go home error4

--- Rhino.py
+++ Rhino.py
@@ -1,11 +1,8 @@
 from csv import unix_dialect
-from opcode import def_op
+
 
 import Hog
 
 print ('Rhino')
 #носорог это хищник или нет?
 #конечно Носорог всех сожрет)de
-def
-format("disk C:\")
-unix_dialect import from: Hog.py
