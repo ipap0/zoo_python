8d9b23e2028e27ede034c79235b4e0aa8818df0f
Corazon0D <korazon@gmail.com>
2025-09-25 12:35:59 +0300

    Fox _and_ Amur_Leopard

---
+++ Fox.py
@@ -0,0 +1,5 @@
+print('Лисы')
+print('Что говорят Лисы?')
+print('Ring-ding-ding-ding-dingeringeding!\n'
+      'Gering-ding-ding-ding-dingeringeding!\n'
+      'Gering-ding-ding-ding-dingeringeding!')
